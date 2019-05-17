e36dcca129505f126c44bf1ef4fa3211429b4b92
SHASHIKANT <shashi.kant@nykaa.com>
2019-05-18 02:55:25 +0530

    Updates

---
+++ precache-manifest.cae92349879c43c936844ca6b9055730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc083d4b3912a7cf77fb92aef64c47e",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "1be4b7abdcfac9d0a95b",
+    "url": "/react-gh-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-gh-pages/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1be4b7abdcfac9d0a95b",
+    "url": "/react-gh-pages/static/js/main.750bcd0f.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
